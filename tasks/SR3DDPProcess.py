b5e8c8bd28fda842a06a0e80e996f5dc9e3ad4ca
Adrián Moreno <90756596+Descalabreitor@users.noreply.github.com>
2024-06-11 11:15:56 +0100

    Sincronization

--- tasks/SR3DDPProcess.py
+++ tasks/SR3DDPProcess.py
@@ -4,33 +4,12 @@ from torch.distributed import destroy_process_group
 from torch.utils.data import random_split
 from torchvision.transforms.v2 import Compose, RandomApply, GaussianBlur, RandomEqualize
 
-from models.SRDIFFBuilder import SRDiffBuilder
+from models.SR3Builder import SR3Builder
 from SRDiffTrainerDDP import SRDiffTrainerDDP
 import utils.DDP_utils as ddp_utils
 from Dataset.AerialDataset import AerialDataset
 import torch.multiprocessing as mp
 
-
-def set_up_data(hyperparams, dataset_dir, sat_dataset_dir, world_size):
-    lr_size = 64
-    hr_size = 256
-    dataset_dir = dataset_dir
-
-    transforms = Compose(
-        [RandomApply(transforms=[GaussianBlur(7)], p=0.5),
-         RandomEqualize()]
-    )
-
-    dataset = AerialDataset(dataset_dir, lr_size, hr_size, data_augmentation=None, aux_sat_prob=0.4,
-                            sat_dataset_path=sat_dataset_dir)
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [0.6, 0.2, 0.2],
-                                                            generator=torch.Generator().manual_seed(420))
-
-    train_dataloader = ddp_utils.prepare_data(train_dataset, hyperparams['batch_size'], world_size)
-    val_dataloader = ddp_utils.prepare_data(val_dataset, hyperparams['batch_size'], world_size)
-    test_dataloader = ddp_utils.prepare_data(test_dataset, hyperparams['batch_size'], world_size)
-
-    return train_dataloader, val_dataloader, test_dataloader
 
 
 def set_up_trainer(hyperparams, rank):
--- utils/DDP_utils.py
+++ utils/DDP_utils.py
@@ -21,3 +21,25 @@ def prepare_data(dataset, batch_size, n_workers=2):
         num_workers=n_workers,
         sampler=DistributedSampler(dataset)
     )
+
+def set_up_data(hyperparams, dataset_dir, sat_dataset_dir, world_size):
+    lr_size = 64
+    hr_size = 256
+    dataset_dir = dataset_dir
+
+    transforms = Compose(
+        [RandomApply(transforms=[GaussianBlur(7)], p=0.5),
+         RandomEqualize()]
+    )
+
+    dataset = AerialDataset(dataset_dir, lr_size, hr_size, data_augmentation=None, aux_sat_prob=0.4,
+                            sat_dataset_path=sat_dataset_dir)
+    train_dataset, val_dataset, test_dataset = random_split(dataset, [0.6, 0.2, 0.2],
+                                                            generator=torch.Generator().manual_seed(420))
+
+    train_dataloader = ddp_utils.prepare_data(train_dataset, hyperparams['batch_size'], world_size)
+    val_dataloader = ddp_utils.prepare_data(val_dataset, hyperparams['batch_size'], world_size)
+    test_dataloader = ddp_utils.prepare_data(test_dataset, hyperparams['batch_size'], world_size)
+
+    return train_dataloader, val_dataloader, test_dataloader
+
